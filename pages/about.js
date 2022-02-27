ec08bcc65361203ec9e6b70e87df9e2b7bbdb769
Sameer <Sameer@Nitins-MacBook-Pro.local>
2022-02-27 23:57:56 +0530

    pages_and_routing

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+        <div>
+        <h1>About</h1>
+        <p>Versions 1.0.0</p>
+        </div>
+    )
+       
+    
+}
---
+++ pages/events/index.jsx
@@ -0,0 +1,9 @@
+export default function EventsPage() {
+    return (
+        <div>
+        <h1>Events page</h1>
+        </div>
+    )
+       
+    
+}
